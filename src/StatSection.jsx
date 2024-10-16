e95344cc9f96acb7ec8272141925d2ffc0c63bb2
mehrbawan <m.bano.awan@gmail.com>
2024-10-16 03:28:15 -0400

    gif

--- src/StatSection.jsx
+++ src/StatSection.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 
 const StatSection = ({ title, data, excludedSet, toggleExclude }) => {
+  const isTypeSection = title.toLowerCase() === "types"; // Check if the section is for Types
+
   return (
     <div className="stats-col">
       <h2>{title}</h2>
-      {data.map((item, index) => {
-        const isTypeSection = title.toLowerCase() === "types";  // Check if the section is for Types
-        return (
-          <button
-            key={index}
-            type={`${isTypeSection ? item.name : ''}`}
-            className="attributeButton" // Conditional class for type
-            onClick={() => toggleExclude(item.name)}
-            {...(isTypeSection && { "data-type": item.name })} // Conditionally add 'data-type' attribute for types
-          >
-            {item.name}
-          </button>
-        );
-      })}
-      
+      {data.map((item, index) => (
+        <button
+          key={index}
+          type={isTypeSection ? item.name : ''}
+          className="attributeButton"
+          onClick={() => toggleExclude(item.name)}
+          {...(isTypeSection && { "data-type": item.name })} // Conditionally add 'data-type' attribute for types
+        >
+          {item.name}
+        </button>
+      ))}
+
       <div className="excluded-types-container">
         <h3>Excluding:</h3>
         {excludedSet.size > 0 ? (
-          <ul>
+          <div className="excluded-buttons">
             {Array.from(excludedSet).map((item, index) => (
-              <li key={index}>{item}</li>
+              <button
+                key={index}
+                className="excludedButton"
+                onClick={() => toggleExclude(item)} // Remove from exclusion list when clicked
+              >
+                {item} &times; {/* Add '×' symbol to indicate removal */}
+              </button>
             ))}
-          </ul>
+          </div>
         ) : (
           <p>No {title.toLowerCase()} excluded yet.</p>
         )}
